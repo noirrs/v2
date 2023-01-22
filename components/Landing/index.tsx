1623297e36ae815ad09bd59e2008f7c6043a71a9
Taha Kaçmaz <hi@noir.land>
2023-01-23 00:48:17 +0300

    fixed: bg color bug

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -13,7 +13,7 @@ export default function Landing() {
 		window && Setter(window);
 	}, []);
 	return (
-		<div className="w-screen h-screen ">
+		<div className="w-screen h-screen bg-black">
 			<CommandPalette pg={[{ title: "Home", href: "/" }]} />
 			<div className="w-screen h-screen lg:flex lg:flex-col lg:justify-center absolute">
 				<div className="flex flex-col lg:flex-row justify-evenly gap-x-40  relative backdrop-blur-0 lg:gap-y-0 gap-y-10 mt-20 lg:mt-0 mx-1 lg:mx-0">
