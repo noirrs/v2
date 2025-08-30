f4f68cfc90cbec17638ded9cc112e4b2080ae969
Taha <noirrsw@gmail.com>
2025-08-30 22:17:48 +0300

    fix

--- components/Loading/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="w-screen h-screen flex flex-col justify-center">
-      <div className="loader mx-auto"></div>
-    </div>
-  );
-}
